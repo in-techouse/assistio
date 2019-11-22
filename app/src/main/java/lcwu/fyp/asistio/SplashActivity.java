1995e75edf9645cb51e930f9decb58827de26082
Areej <areejikram3@gmail.com>
2019-11-22 12:01:04 +0500

    Dependencies Updated

--- app/src/main/java/lcwu/fyp/asistio/SplashActivity.java
+++ app/src/main/java/lcwu/fyp/asistio/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
 //        splash.setAnimationType(Splash.AnimationType.TYPE_2);
         splash.perform();
 
-        new CountDownTimer(2000, 1000) {
+        new CountDownTimer(3000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
