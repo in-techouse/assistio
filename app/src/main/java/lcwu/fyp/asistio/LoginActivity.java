ae38d68b97f9f2e8b2838cebc447003192a728a2
Areej <areejikram3@gmail.com>
2019-11-23 11:13:01 +0500

    working on firebase

--- app/src/main/java/lcwu/fyp/asistio/LoginActivity.java
+++ app/src/main/java/lcwu/fyp/asistio/LoginActivity.java
@@ -2,16 +2,21 @@ package lcwu.fyp.asistio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnLogin;
-
+    TextView go_to_registration;
     EditText edtEmail, edtPassword;
+    String strEmail , strPassword;
 
 
     @Override
@@ -22,8 +27,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnLogin = findViewById(R.id.btnLogin);
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
+        go_to_registration = findViewById(R.id.go_to_registration);
 
         btnLogin.setOnClickListener(this);
+        go_to_registration.setOnClickListener(this);
     }
 
     @Override
@@ -32,24 +39,43 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         switch (id) {
             case R.id.btnLogin: {
-                String strEmail = edtEmail.getText().toString();
-                String strPassword = edtPassword.getText().toString();
+                 strEmail = edtEmail.getText().toString();
+                 strPassword = edtPassword.getText().toString();
 
-                if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
-                    edtEmail.setError("Enter a valid Email");
-                } else {
-                    edtEmail.setError(null);
-                }
-                if (strPassword.length() < 6) {
-                    edtPassword.setError("Enter valid password");
-                } else {
-                    edtPassword.setError(null);
-                }
+                   boolean flag = isValid();
+                    if (flag) {
+                        //Firebase
+                        FirebaseAuth auth = FirebaseAuth.getInstance();
+                        auth.signInWithEmailAndPassword(strEmail,strPassword);
+
+                    }
+                    break;
+            }
+            case R.id.go_to_registration:{
+                Intent it = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(it);
                 break;
             }
-
         }
 
     }
+
+    private boolean isValid(){
+        boolean flag =true;
+        if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+            edtEmail.setError("Enter a valid Email");
+            flag= false;
+        } else {
+            edtEmail.setError(null);
+        }
+        if (strPassword.length() < 6) {
+            edtPassword.setError("Enter valid password");
+            flag=false;
+        } else {
+            edtPassword.setError(null);
+        }
+        return flag;
+    }
+
 
 }
