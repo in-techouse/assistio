41c0b19b6e8114eefd6a5fc938e16dde2b592b12
DELL <sadiaabbas1212@gmail.com>
2019-12-28 13:15:13 +0500

    working on nav header

--- app/src/main/java/lcwu/fyp/asistio/Dashboard.java
+++ app/src/main/java/lcwu/fyp/asistio/Dashboard.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,18 +23,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import java.util.jar.Attributes;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import lcwu.fyp.asistio.Director.Helpers;
 import lcwu.fyp.asistio.Director.Session;
 import lcwu.fyp.asistio.model.User;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
     private Helpers helpers;
     private User user;
     private Session session;
-
+    private CircleImageView profile_image;
+    private TextView profile_name;
+    private TextView profile_email;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +50,50 @@ public class Dashboard extends AppCompatActivity {
         helpers = new Helpers();
         session = new Session(Dashboard.this);
         user = session.getUser();
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+
+       profile_image = header.findViewById(R.id.profile_image);
+       profile_name = header.findViewById(R.id.profile_name);
+       profile_email = header.findViewById(R.id.profile_email);
+
+       profile_name.setText(user.getFirst_Name()+ " "+ user.getLast_Name());
+       profile_email.setText(user.getEmail());
     }
 
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.nav_home:{
+                break;
+            }
+            case R.id.nav_autoreply:{
+                break;
+            }
+            case R.id.nav_smssch:{
+                break;
+            }
+
+            case R.id.nav_speech:{
+                break;
+            }
+            case R.id.nav_logout:{
+                break;
+            }
+
+
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
