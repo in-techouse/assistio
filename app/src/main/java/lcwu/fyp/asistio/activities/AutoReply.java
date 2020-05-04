400eb40008b3150a1d14fa31cf6a316462e1efbf
Areej <areejikram3@gmail.com>
2020-05-04 05:41:12 +0500

    working on autorply

--- app/src/main/java/lcwu/fyp/asistio/activities/AutoReply.java
+++ app/src/main/java/lcwu/fyp/asistio/activities/AutoReply.java
@@ -2,17 +2,28 @@ package lcwu.fyp.asistio.activities;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import lcwu.fyp.asistio.R;
 
-public class AutoReply extends AppCompatActivity {
+public class AutoReply extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText message;
+    private EditText reply_message;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_reply);
+
+        message= findViewById(R.id.message);
+        save = findViewById(R.id.save);
+        reply_message = findViewById(R.id.reply_message);
     }
 
     @Override
@@ -29,5 +40,10 @@ public class AutoReply extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/lcwu/fyp/asistio/activities/SpeechNotes.java
+++ app/src/main/java/lcwu/fyp/asistio/activities/SpeechNotes.java
@@ -2,17 +2,29 @@ package lcwu.fyp.asistio.activities;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import lcwu.fyp.asistio.R;
 
-public class SpeechNotes extends AppCompatActivity {
+public class SpeechNotes extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText notes;
+    private ImageView record_ur_voice;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech_notes);
+
+        notes = findViewById(R.id.notes);
+        record_ur_voice= findViewById(R.id.record_ur_voice);
+        save = findViewById(R.id.save);
     }
 
     @Override
@@ -29,5 +41,10 @@ public class SpeechNotes extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
